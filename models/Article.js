47a567a335270ba877710272be085e540d95f389
Joshua Coleman <procoleman77@outlook.com>
2019-12-15 11:41:10 -0500

    added back basic structure

--- models/Article.js
+++ models/Article.js
@@ -1,6 +1,5 @@
 var mongoose = require("mongoose");
 
-// Save a reference to the Schema constructor
 var Schema = mongoose.Schema;
 
 // Using the Schema constructor, create a new UserSchema object
@@ -19,10 +18,10 @@ var ArticleSchema = new Schema({
   // `note` is an object that stores a Note id
   // The ref property links the ObjectId to the Note model
   // This allows us to populate the Article with an associated Note
-  note: [{
+  note: {
     type: Schema.Types.ObjectId,
     ref: "Note"
-  }]
+  }
 });
 
 // This creates our model from the above schema, using mongoose's model method
--- models/Note.js
+++ models/Note.js
@@ -10,7 +10,7 @@ var NoteSchema = new Schema({
   title: String,
   // `body` is of type String
   body: String,
-  required: true
+  
 });
 
 // This creates our model from the above schema, using mongoose's model method
